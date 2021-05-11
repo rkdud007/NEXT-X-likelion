1ec0f5cf7a64179221555d1b6ac1119b55202bff
rkdud007 <gayeongparkk@gmail.com>
2021-05-11 14:38:12 +0900

    8과제

---
+++ blog/blogapp/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by("due_date")
+
+    return render(request, "home.html", {"posts": posts})
+
+
+def new(request):
+    if request.method == "POST":
+        new_post = Post.objects.create(
+            title=request.POST["title"],
+            content=request.POST["content"],
+            due_date=request.POST["due_date"],
+        )
+        return redirect("detail", new_post.pk)
+    return render(request, "new.html")
+
+
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.method == "POST":
+        Comment.objects.create(post=post, content=request.POST["content"])
+        return redirect("detail", post_pk)
+
+    return render(request, "detail.html", {"post": post})
+
+
+def edit(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+
+    if request.method == "POST":
+        Post.objects.filter(pk=post_pk).update(
+            title=request.POST["title"],
+            content=request.POST["content"],
+            due_date=request.POST["due_date"],
+        )
+        return redirect("detail", post.pk)
+    return render(request, "edit.html", {"post": post})
+
+
+def delete(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    post.delete()
+
+    return redirect("home")
+
+
+def delete_comment(request, post_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect("detail", post_pk)
